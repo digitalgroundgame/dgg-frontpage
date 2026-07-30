d36a60479f4307eaa214f800b58ec5f0037f4582
Evan F <3375094+evan6seven@users.noreply.github.com>
2026-07-30 21:31:28 +0000

    VoteFromAbroad.org OG title

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -4,13 +4,13 @@ import { VoteRedirect } from "./vote-redirect";
 const destination = "https://voteabroad.org/ccJP-DGG-2026";
 
 export const metadata: Metadata = {
-  title: "Register to Vote From Abroad!",
+  title: "Register to Vote From Abroad | DGG",
   description: "",
   alternates: {
     canonical: "/vote",
   },
   openGraph: {
-    title: "Register to Vote From Abroad!",
+    title: "Request Your Absentee Ballot | VoteFromAbroad.org",
     description: "",
     url: "/vote",
     siteName: "Digital Ground Game",
@@ -20,13 +20,13 @@ export const metadata: Metadata = {
         url: "/uploads/vote-from-abroad-cta-cover.webp",
         width: 800,
         height: 418,
-        alt: "Register to vote from abroad with Digital Ground Game",
+        alt: "Register to vote from abroad with VoteFromAbroad.org",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Register to Vote From Abroad!",
+    title: "Request Your Absentee Ballot | VoteFromAbroad.org",
     description: "",
     images: ["/uploads/vote-from-abroad-cta-cover.webp"],
   },
